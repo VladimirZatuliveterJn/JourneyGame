004b72757c255aed76cf3157f1406f8ed581c0ee
Vladimir Zatuliveter <vladimir.zatuliveter@gmail.com>
2024-11-04 14:15:00 +0600

    New stamina system

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
@@ -5,6 +6,9 @@ public class MovePlayer : MonoBehaviour
     private float _speed;
     public float walkspeed = 0.05f;
     public float runspeed = 0.15f;
+
+    private float _stamina = 100f;
+    public float _staminaspeed = 2f;
 
     void Update()
     {
@@ -16,9 +20,27 @@ public class MovePlayer : MonoBehaviour
             this.transform.position += this.transform.right * _speed;
         if (Input.GetKey(KeyCode.A))
             this.transform.position -= this.transform.right * _speed;
-        if (Input.GetKey(KeyCode.LeftShift))
+        
+        if (Input.GetKey(KeyCode.LeftShift) && _stamina > 1)
+        {
             _speed = runspeed;
+            _stamina -= _staminaspeed * Time.deltaTime;
+        }
+            
         else
+        {
             _speed = walkspeed;
+            Invoke("IncreaseStamina", 3);
+        }
+        Debug.Log(_stamina);
+    }
+
+    private void IncreaseStamina()
+    {
+        if (_stamina < 100f)
+            _stamina += 1f * Time.deltaTime;
+        if (_stamina > 100f)
+            _stamina = 100f;
     }
 }
+
