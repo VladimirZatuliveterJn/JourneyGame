efe088154c6b1d6f07ce229084550053d6a0a542
Vladimir Zatuliveter <vladimir.zatuliveter@gmail.com>
2024-11-05 13:49:33 +0600

    Movement with physics

--- Assets/Scripts/MovePlayer.cs
+++ Assets/Scripts/MovePlayer.cs
@@ -1,65 +1,75 @@
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
     private float _speed;
-    public float walkspeed = 0.05f;
-    public float runspeed = 0.15f;
+    public float walkspeed = 500f;
+    public float runspeed = 1000f;
 
     private float _stamina = 100f;
     public float _staminaspeed = 2f;
+    private bool _canRest; // Can increase stamina
 
     bool isGrounded;
+    public float JumpForce = 200f;
     private Rigidbody Rb;
 
     void Start()
     {
         Rb = GetComponent<Rigidbody>();
     }
-    void Update()
+    
+    private void FixedUpdate() 
     {
-        if (Input.GetKey(KeyCode.W))
-            this.transform.position += this.transform.forward * _speed;
-        if (Input.GetKey(KeyCode.S))
-            this.transform.position -= this.transform.forward * _speed;
-        if (Input.GetKey(KeyCode.D))
-            this.transform.position += this.transform.right * _speed;
-        if (Input.GetKey(KeyCode.A))
-            this.transform.position -= this.transform.right * _speed;
-        
-        if (Input.GetKey(KeyCode.LeftShift) && isGrounded && _stamina > 1)
-        {
-            _speed = runspeed;
-            _stamina -= _staminaspeed * Time.deltaTime;
-        }
-        else
-        {
-            _speed = walkspeed;
-            Invoke("IncreaseStamina", 3);
-        }
-        Debug.Log(_stamina);
+        // Check movement direction and speed
+        CheckDir();
 
+        // Calculate movement input
+        float h = Input.GetAxis("Horizontal") * _speed * Time.fixedDeltaTime;
+        float v = Input.GetAxis("Vertical") * _speed * Time.fixedDeltaTime;
+
+        // Move the player
+        Rb.velocity = transform.TransformDirection(new Vector3(h, Rb.velocity.y, v));
+
+        // Jump if grounded and has stamina
         if (Input.GetKey(KeyCode.Space) && isGrounded && _stamina > 1)
         {
-            Rb.AddForce(new Vector3(0, 200f, 0), ForceMode.Impulse);
+            Rb.AddForce(Vector3.up * JumpForce, ForceMode.Impulse);
             _stamina--;
             isGrounded = false;
         }
 
-        Physics.gravity = new Vector3(0, -9.8F, 0);
-    }
-    void OnCollisionStay()
-    	{
-    		isGrounded = true;
-    	}
+        // Regenerate stamina
+        if (_canRest)
+        {
+            if (_stamina < 100f)
+                _stamina += 1f * Time.deltaTime;
+            if (_stamina > 100f)
+                _stamina = 100f;
+        }
 
-    private void IncreaseStamina()
+        Debug.Log(_stamina);
+    }
+
+    void OnCollisionStay()
     {
-        if (_stamina < 100f)
-            _stamina += 1f * Time.deltaTime;
-        if (_stamina > 100f)
-            _stamina = 100f;
+        isGrounded = true;
+    }
+
+    private void CheckDir() // Check direction & speed of move
+    {
+        // Adjust speed and stamina based on run/walk condition
+        if (Input.GetKey(KeyCode.LeftShift) && isGrounded && _stamina > 1)
+        {
+            _speed = runspeed;
+            _stamina -= _staminaspeed * Time.deltaTime;
+            _canRest = false;
+        }
+        else
+        {
+            _speed = walkspeed;
+            _canRest = true;
+        }
     }
 }
 
